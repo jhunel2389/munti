ab74911bcbbb9b54832d664657929e47c06370e1
Jhunel <jhunel_2389@yahoo.com>
2016-11-04 18:40:37 +0800

    update
    update

--- app/Http/Controllers/RecordController.php
+++ app/Http/Controllers/RecordController.php
@@ -16,4 +16,9 @@ class RecordController extends Controller
     	$userInfo = App::make("App\Http\Controllers\GlobalController")->userInfoList(Auth::User()['id']);
 		return View::Make("records.record")->with("userInfo",$userInfo)->with('mt','re');
     }
+
+    function addRecord()
+    {
+    	
+    }
 }
--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -1,9 +1,7 @@
 <?php 
 namespace App\Http\Controllers;
 
-use App\Models\Info;
 use App\Models\User;
-use App\Models\PasswordRecovery;
 use Validator;
 use Response;
 use Auth;
